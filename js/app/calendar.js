ecf386063135ad0a6ad61d466b06c0121a38e186
Charlie Ferris <charlieferris15@gmail.com>
2020-01-07 10:49:51 +0000

    updated calendar tasks

--- js/app/calendar.js
+++ js/app/calendar.js
@@ -45,7 +45,7 @@ function setDisplay(month, year) {
     }
     day--;
 
-    calendar.html(`<div style="width: 714px" class="bar">${months[date.getMonth()]}, ${date.getFullYear()}</div>`);
+    calendar.html(`<button onclick="lastMonth()">prev</button><div style="width: 555px" class="bar">${months[date.getMonth()]}, ${date.getFullYear()}</div><button onclick="nextMonth()">next</button>`);
     calendar.append(`<div class="bar">Monday</div><div class="bar">Tuesday</div><div class="bar">Wednesday</div>`);
     calendar.append(`<div class="bar">Thursday</div><div class="bar">Friday</div><div class="bar">Saturday</div>`);
     calendar.append(`<div class="bar">Sunday</div>`);
@@ -83,11 +83,40 @@ function setDisplay(month, year) {
         calendar.append(`<div class="day" style="color: grey;${style}">Date: ${i}</div>`)
     }
 
+
+    //Render tasks
+    let colours = ['task-red', 'task-yellow', 'task-green'];
+    let tasks = getSessJSON('taskDat').tasks;
+    for(let i = 0; i < tasks.length; i++){
+        let d = tasks[i].due_date;
+        if(!d)
+            continue;
+        let date = new Date(Number(d));
+
+        if(date.getFullYear() === year && date.getMonth() === month){
+            $('#'+date.getDate()).append('<div class="'+colours[tasks[i].status]+' blob"></div>');
+        }
+    }
+    let subTasks = getSessJSON('subTaskDat');
+    for(let i in Object.keys(subTasks)){
+        let tsk = subTasks[Object.keys(subTasks)[i]];
+        let d = tsk.due_date;
+        if(!d)
+            continue;
+        let date = new Date(Number(d));
+
+        if(date.getFullYear() === year && date.getMonth() === month){
+            $('#'+date.getDate()).append('<div class="'+colours[tsk.status]+' blob"></div>');
+        }
+    }
+
 }
 
 //IF(due_date === currently rendering day)
 //      Add a circle with colour of "status" to the elm
 
 //DATA:
-//{"exists":true,"tasks":[{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315231445","task_id":"8315231445","details":"Create the settings menus and databases","due_date":null,"parent_id":null,"status":1,"title":"Settings"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315326618","task_id":"8315326618","details":"Allow users to be invited to boards to work together","due_date":null,"parent_id":null,"status":0,"title":"Colaboration"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315522080","task_id":"8315522080","details":"-","due_date":null,"parent_id":null,"status":0,"title":"Set up servers and push"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8317180897","task_id":"8317180897","details":"-","due_date":null,"parent_id":null,"status":1,"title":"Login and signup pages"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8429355292","task_id":"8429355292","details":"Calendar.","due_date":"1578441600000","parent_id":null,"status":1,"title":"Calendar"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8433792433","task_id":"8433792433","details":"-","due_date":null,"parent_id":null,"status":2,"title":"Comments fs8dg9fbvf"}],"id":"eaaa5e12"}
-//{"8429355292":{"details":"Calendar.","title":"Calendar","is_top":true,"task_id":"8429355292","status":1,"parent_id":null,"due_date":"1578441600000"},"8538905000":{"details":"-","title":"Unnamed Task","is_top":false,"task_id":"8538905000","status":1,"parent_id":"8429355292","due_date":"1578441600000"}}
+let a = {"exists":true,"tasks":[{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315231445","task_id":"8315231445","details":"Create the settings menus and databases","due_date":null,"parent_id":null,"status":1,"title":"Settings"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315326618","task_id":"8315326618","details":"Allow users to be invited to boards to work together","due_date":null,"parent_id":null,"status":0,"title":"Colaboration"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8315522080","task_id":"8315522080","details":"-","due_date":null,"parent_id":null,"status":0,"title":"Set up servers and push"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8317180897","task_id":"8317180897","details":"-","due_date":null,"parent_id":null,"status":1,"title":"Login and signup pages"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8429355292","task_id":"8429355292","details":"Calendar.","due_date":"1578441600000","parent_id":null,"status":1,"title":"Calendar"},{"board_id":"eaaa5e12","is_top":true,"root_node_id":"8433792433","task_id":"8433792433","details":"-","due_date":null,"parent_id":null,"status":2,"title":"Comments fs8dg9fbvf"}],"id":"eaaa5e12"}
+let b = {"8429355292":{"details":"Calendar.","title":"Calendar","is_top":true,"task_id":"8429355292","status":1,"parent_id":null,"due_date":"1578441600000"},"8538905000":{"details":"-","title":"Unnamed Task","is_top":false,"task_id":"8538905000","status":1,"parent_id":"8429355292","due_date":"1578441600000"}}
+saveSess('taskDat', a);
+saveSess('subTaskDat', b);
